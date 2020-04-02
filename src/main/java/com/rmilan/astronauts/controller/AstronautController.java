50b721a9a837b6e9d5b889e9f1cd326165b6494d
restarmilan <restarmilan@gmail.com>
2020-04-02 19:39:09 +0200

    add basic methods for get/post mapping

--- src/main/java/com/rmilan/astronauts/controller/AstronautController.java
+++ src/main/java/com/rmilan/astronauts/controller/AstronautController.java
@@ -1,21 +1,15 @@
 package com.rmilan.astronauts.controller;
 
 import com.rmilan.astronauts.entity.Astronaut;
-import com.rmilan.astronauts.entity.Mission;
 import com.rmilan.astronauts.repository.AstronautRepository;
 import com.rmilan.astronauts.repository.MissionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.UnaryOperator;
 
 @RestController
 @RequestMapping("/api")
@@ -29,9 +23,7 @@ public class AstronautController {
 
     @GetMapping("/astronauts")
     public List<Astronaut> getAllAstronauts() {
-
         return astronautRepository.findAll();
-
     }
 
     @GetMapping("astronauts/{id}")
@@ -44,12 +36,10 @@ public class AstronautController {
     @GetMapping("astronauts/name/{name}")
     public List<Astronaut> getAstronautByName(@PathVariable("name") String name) {
         return astronautRepository.findByNameContains(name);
-
     }
 
     @PostMapping(value = "/astronauts", consumes = MediaType.APPLICATION_JSON_VALUE)
     public Astronaut createAstronaut(@Valid @RequestBody Astronaut astronaut) {
         return astronautRepository.save(astronaut);
     }
-
 }
